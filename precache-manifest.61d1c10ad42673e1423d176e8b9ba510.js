7468cac570b602f1bef1b4f00c97d30bb123f3be
Munish Thakur <munish81198@gmail.com>
2020-08-27 20:21:08 +0530

    Updates

---
+++ precache-manifest.61d1c10ad42673e1423d176e8b9ba510.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9c0f7948838238399762917add6205",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "7d157c3240c55e4733cd",
+    "url": "/react-todo/static/css/main.b7b1e59d.chunk.css"
+  },
+  {
+    "revision": "19552f62a6adb6ecda58",
+    "url": "/react-todo/static/js/2.8e63e482.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.8e63e482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d157c3240c55e4733cd",
+    "url": "/react-todo/static/js/main.c5ab1cde.chunk.js"
+  },
+  {
+    "revision": "ce701484895442b2eb85",
+    "url": "/react-todo/static/js/runtime-main.e4e550a5.js"
+  }
+]);
